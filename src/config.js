a8689984403510ac5d1a72cf606c8e398d7b3f3f
Vinicius Pereira <vp.vinicius.pereira@gmail.com>
2025-01-20 17:56:16 +0100

    update config.js with API endpoints

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = "https://p0eexuvlmc.execute-api.us-east-1.amazonaws.com/dev/questions";
-export const WS_ENDPOINT = "wss://nqpzwsni9j.execute-api.us-east-1.amazonaws.com/dev/";
+export const API_ENDPOINT = "https://i48qzzqpch.execute-api.us-east-1.amazonaws.com/dev/questions";
+export const WS_ENDPOINT = "wss://tli141ej8j.execute-api.us-east-1.amazonaws.com/dev/";
