8c9ae1b211538c876e10fec7ee8e781bac8b8f07
Vinicius Pereira <vp.vinicius.pereira@gmail.com>
2025-02-08 17:19:57 +0100

    updated config.js with APIs URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = "https://i48qzzqpch.execute-api.us-east-1.amazonaws.com/dev/questions";
-export const WS_ENDPOINT = "wss://tli141ej8j.execute-api.us-east-1.amazonaws.com/dev/";
+export const API_ENDPOINT = "https://g2jl4yt00l.execute-api.us-east-1.amazonaws.com/dev/questions";
+export const WS_ENDPOINT = "wss://ibwy5x0g6f.execute-api.us-east-1.amazonaws.com/dev/";
